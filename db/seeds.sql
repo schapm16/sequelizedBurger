2bdb5535c95b98398693f96eefb75a9d812af17f
Stephen Chapman <schapm16@gmail.com>
2017-12-17 22:25:02 +0000

    Deleted legacy DB schema and seeds file from the first burger hwk assignment

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-USE burgers_db;
-
-INSERT INTO burgerSeq (burger_name) VALUES ('bacon cheesburger');
-INSERT INTO burgerSeq (burger_name) VALUES ('veggie burger');
-INSERT INTO burgerSeq (burger_name) VALUES ('jalepeno western burger');
-
